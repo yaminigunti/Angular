89f59bf8908d6fb095cba8e001cdd9582e755346
yaminigunti <60088148+yaminigunti@users.noreply.github.com>
2020-01-25 12:31:45 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -6,22 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private FirstNumber:number=null;
-  private SecondNumber:number=null;
-  private Result:number=null;
+  private message:string=null;
+  private version :number=null;
+  private topics:string[]=null;
+  private topicname:string=null;
+  private index:number=null;
+  
+  public  constructor(){
+    this.message="welcome Yamini!";
+    this.version=8;
+    this.topics=['Data Binding','Forms','Http','Routing'];
+  }
+  public addTopic(currentTopic:string):boolean {
+    if(currentTopic.length==0){
+      alert('empty items cannot be added');
+      return false;
+    }
+   
+    for(var topic of this.topics){
+      if(currentTopic==topic){
+        alert('topic already exists !!!!! enter new topic')
+      }
+    }
+    this.topics.push(currentTopic) 
+  }
 
+  public deleteTopic(idx:number):void {
+     this.topics.splice(idx,1);  
+  } 
+  public deleteTopicName(currentTopic:string):void{
+for(var topic of this.topics){
+if(currentTopic==topic){
+  this.index=this.topics.indexOf(currentTopic);
+  this.topics.splice(this.index,1);
+}  
+}
+alert('item is deleted');
+}
 
-public getAdd():void {
-  this.Result=this.FirstNumber+this.SecondNumber;
-}
-public getSub():void {
-  this.Result=this.FirstNumber-this.SecondNumber;
-}
-public getMul():void {
-  this.Result=this.FirstNumber*this.SecondNumber;
-}
-public getDiv():void {
-  this.Result=this.FirstNumber/this.SecondNumber;
+  public searchTopic(currentTopic:string):void {
+    for(var topic of this.topics){
+    if(currentTopic==topic) {
+      this.index=this.topics.indexOf(currentTopic);
+      alert('topic present at'+(this.index+1));
+    } 
+  }
+  alert('topic is not present')
+  }
 
-}
 }
